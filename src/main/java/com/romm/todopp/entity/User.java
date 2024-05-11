d958cc1de90f0ea7c01c038e282115c78813490a
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-05-11 00:46:08 -0300

    fix: missing taskLists attribute on User entity

--- src/main/java/com/romm/todopp/entity/User.java
+++ src/main/java/com/romm/todopp/entity/User.java
@@ -6,11 +6,14 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -30,25 +33,33 @@ public class User implements UserDetails {
     private String username;
     // not validating right now
     private String email;
+
     @Column(nullable = false)
     private String password;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
+    private List<TaskList> taskLists;
     
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority("ROLE_USER"));
     }
+
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
+    
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
+
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
+
     @Override
     public boolean isEnabled() {
         return true;
