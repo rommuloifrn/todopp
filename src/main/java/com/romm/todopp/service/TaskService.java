8267897ba3415ed9702b832bd5d40bfbfce0d764
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-05-14 19:23:32 -0300

    feat: user ownership checking on TaskService operations

--- src/main/java/com/romm/todopp/service/TaskService.java
+++ src/main/java/com/romm/todopp/service/TaskService.java
@@ -10,7 +10,6 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.romm.todopp.DTO.TaskDTO;
 import com.romm.todopp.entity.Task;
-import com.romm.todopp.entity.User;
 import com.romm.todopp.repository.TaskRepository;
 import com.romm.todopp.repository.UserRepository;
 
@@ -19,28 +18,29 @@ public class TaskService {
     
     @Autowired TaskRepository taskRepository;
     @Autowired UserRepository userRepository;
+    @Autowired AuthenticationService authenticationService;
+    @Autowired AuthorizationService authorizationService;
 
     public Task create(Task task, Long taskListId) {
         task.setCreatedAt(Instant.now());
-        User defaultUser = userRepository.findById(Long.valueOf(1)).get();
-        task.setOwner(defaultUser);
+        task.setOwner(authenticationService.getPrincipal());
         return taskRepository.save(task);
     }
 
     public Task read(Long id) throws ResponseStatusException {
-        Task task = findOr404(id);
+        Task task = accessAsUser(id);
         return task;
     }
 
     //refatorar isso aqui pra receber a Task direto depois...
     public void update(TaskDTO data, Long id) throws ResponseStatusException {
-        Task task = findOr404(id);
+        Task task = accessAsUser(id);
         if (data.title() != null) task.setTitle(data.title());
         taskRepository.save(task);
     }
 
     public Task delete(Long id) throws ResponseStatusException { 
-        Task task = findOr404(id);
+        Task task = accessAsUser(id);
         taskRepository.delete(task);
         return task;
     }
@@ -50,9 +50,15 @@ public class TaskService {
     }
 
     public Task toggleFinish(Long id) throws ResponseStatusException {
-        Task task = findOr404(id);
+        Task task = accessAsUser(id);
         task.setFinished(!task.isFinished());
         taskRepository.save(task);
+        return task;
+    }
+
+    public Task accessAsUser(Long id) throws ResponseStatusException {
+        var task = findOr404(id);
+        authorizationService.requestUserIsOwnerOrError(task);
         return task;
     }
 
