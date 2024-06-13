7488ba9540a9509d2149dc38a52ada7b727659fc
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-06-13 12:15:21 -0300

    feat: add some light colors and use them on "read list" page

--- src/main/frontend/tailwind.config.js
+++ src/main/frontend/tailwind.config.js
@@ -7,6 +7,16 @@ module.exports = {
     theme: {
       extend: {
         colors: {
+          'floor': "#D7D7D7",
+          'element': "#F3F3F3",
+          'element-h': "#EBEBEB",
+          'handle': "#828282",
+          'handle-h': "#565656",
+
+          'speech': "#252525",
+          'speech-h': "#4E4E4E",
+
+          // dark ones
           'floor-dark': "#252525",
           'element-dark':"#101010",
           'element-dark-hover':"#080808",
